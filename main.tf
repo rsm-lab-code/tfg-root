83552c9c9bb9002d85e1d6f2a28a18a1a764a188
rsm-lab-code@rsmus.com <rsm-lab-code@rsmus.com>
2025-05-20 18:31:10 +0000

    dummy test

--- main.tf
+++ main.tf
@@ -54,6 +54,12 @@ module "inspection_vpc" {
   
   # Firewall endpoint IDs
   firewall_endpoint_ids = module.network_firewall.firewall_endpoint_ids
+
+  # VPC CIDR BLOCKS
+  dev_vpc1_cidr     = module.vpc.vpc_cidr
+  #dev_vpc2_cidr     = module.dev_vpc2.vpc_cidr
+  #nonprod_vpc1_cidr = module.nonprod_vpc1.vpc_cidr
+  #nonprod_vpc2_cidr = module.nonprod_vpc2.vpc_cidr
   
   providers = {
     aws.delegated_account_us-west-2 = aws.delegated_account_us-west-2
@@ -109,7 +115,7 @@ module "vpc" {
   source = "github.com/rsm-lab-code/tfg-spoke//dev_vpc1?ref=main"
 
   #VPC name
-   vpc_name = "dev_vpc1"
+  vpc_name = "dev_vpc1"
 
   # Account IDs
   delegated_account_id = var.delegated_account_id
